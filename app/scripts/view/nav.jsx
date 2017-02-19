e2e7745f4af97f464be891f4bde151277229bd7e
Prem <v.premp@gmail.com>
2017-02-19 08:17:39 -0800

    draft changes for layout

--- app/scripts/view/nav.jsx
+++ app/scripts/view/nav.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Base from 'view/base';
-import count from 'model/count';
 
 'use strict';
 
-export default class Main extends Base {
+export default class Nav extends Base {
 
 	constructor(options) {
         super(options);
@@ -27,6 +26,16 @@ export default class Main extends Base {
         return (
         	<div className="main">
 	           <header className="header"> </header>
+                <nav class="navbar navbar-inverse">
+                  <div class="container-fluid">
+                    <div class="navbar-header">
+                      <a class="navbar-brand" href="#">The Drive</a>
+                    </div>                
+                    <ul class="nav navbar-nav navbar-right">
+                      <li><a href="#"><span class="glyphicon glyphicon-user"></span>Prem</a></li>                     
+                    </ul>
+                  </div>
+                </nav>
 	           <div className="content"> </div>
 	           <footer className="footer"> </footer>
 			</div>
